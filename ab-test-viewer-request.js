c19ab667bac9082c2b5b9ac90b72e7010ade074a
Jordi Nebot <jordi@lovetoknow.com>
2018-12-13 17:34:21 +0100

    Add experiment version cookie and refactor logic

--- ab-test-viewer-request.js
+++ ab-test-viewer-request.js
@@ -1,9 +1,39 @@
 'use strict';
 
-const sourceCoookie = 'X-Source';
-const sourceMain = 'yd-v1';
-const sourceExperiment = 'yd-v2';
-const experimentTraffic = 0.5;
+const returningUserCookie = '_yd_ab_ret';
+const experimentVersion = 'DA6gHVdJ'; // hashids (1,1)
+
+const sourceCookie = '_yd_ab_source';
+const sourceMain = 'AyzHWHBd'; // hashids (1,1,1)
+const sourceExperiment = 'G2bHEHgG'; // hashids (1,1,2)
+
+const experimentTraffic = 0.0;
+
+const hasCookie = (cookies, name, value = null) => {
+    const pattern = value ? `${name}=${value}` : `${name}`;
+    for (let i = 0; i < cookies.length; i++) {
+        if (cookies[i].value.indexOf(pattern) >= 0) {
+            return true;
+        }
+    }
+    return false;
+};
+
+// Sets a new cookie in headers' cookies array or updates its value if
+// it already exists.
+const setCookie = (cookies, name, value) => {
+    let found = false;
+    const cookie = `${name}=${value}`;
+    for (let i = 0; i < cookies.length; i++) {
+        if (cookies[i].value.indexOf(name) >= 0) {
+            found = true;
+            cookies[i].value.replace(new RegExp(`${name}=[^;]+`, 'gi'), cookie);
+        }
+    }
+    if (!found) {
+        cookies.push({key: 'Cookie', value: cookie});
+    }
+};
 
 // Viewer request handler
 exports.handler = (event, context, callback) => {
@@ -11,27 +41,16 @@ exports.handler = (event, context, callback) => {
     const headers = request.headers;
 
     // Look for source cookie
-    if ( headers.cookie ) {
-        for (let i = 0; i < headers.cookie.length; i++) {
-            if (headers.cookie[i].value.indexOf(sourceCoookie) >= 0) {
-                console.log('Source cookie found. Forwarding request as-is');
-                // Forward request as-is
-                callback(null, request);
-                return;
-            }
+    if (hasCookie(headers.cookie, returningUserCookie, experimentVersion)) {
+        if (!hasCookie(headers.cookie, sourceCookie)) {
+            const source =
+                Math.random() < experimentTraffic
+                    ? sourceExperiment
+                    : sourceMain;
+            setCookie(headers.cookie, sourceCookie, source);
         }
+    } else {
+        setCookie(headers.cookie, returningUserCookie, experimentVersion);
     }
-
-    console.log('Source cookie has not been found. Throwing dice...');
-    const source = ( Math.random() < experimentTraffic ) ? sourceExperiment : sourceMain;
-    console.log(`Source: ${source}`)
-
-    // Add Source cookie
-    const cookie = `${sourceCoookie}=${source}`
-    console.log(`Adding cookie header: ${cookie}`);
-    headers.cookie = headers.cookie || [];
-    headers.cookie.push({ key:'Cookie', value: cookie });
-
-    // Forwarding request
     callback(null, request);
 };
